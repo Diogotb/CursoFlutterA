4c9156ccffeecabeebd30374e6cee62e5aa17dd1
Diogotb <digogo@gmail.com>
2024-02-22 17:07:40 -0300

    22/02

--- app_jogo_adivinha/lib/main.dart
+++ app_jogo_adivinha/lib/main.dart
@@ -3,11 +3,9 @@ import 'dart:html';
 import 'package:flutter/material.dart';
 import 'dart:math';
 
-
 void main() {
   runApp(CalculadoraApp());
 }
-
 
 class CalculadoraApp extends StatelessWidget {
   @override
@@ -18,28 +16,44 @@ class CalculadoraApp extends StatelessWidget {
   }
 }
 
-
 class Calculadora extends StatefulWidget {
   @override
   _CalculadoraState createState() => _CalculadoraState();
 }
 
-
 class _CalculadoraState extends State<Calculadora> {
+  //atributos iniciais da classe
   TextEditingController _controllerNumero1 = TextEditingController();
   String _resultado = '';
-  int numeroSorteado = Random(99) as int;
+  int numeroSorteado = Random().nextInt(100) + 1;
+  int cont = 0;
+  bool acertou = false;
 
-  void _adivinhar(){
+  //métodos
+  void _adivinhar() {
     int numero = int.tryParse(_controllerNumero1.text) as int;
-
     setState(() {
-      if(numero != null && numero ){
-
+      cont++;
+      if (numero == numeroSorteado) {
+        _resultado = "Parabêns Vc Acertou!!! Em $cont Tentativas!!!";
+        acertou = true;
+      } else if (numero > numeroSorteado) {
+        _resultado = "Tente um Nº Menor";
+      } else {
+        _resultado = "Tente um Nº Maior";
       }
     });
   }
 
+  void _reiniciar() {
+    cont = 0;
+    numeroSorteado = Random().nextInt(100) + 1;
+    _resultado = "";
+    acertou = false;
+    setState(() {
+      _controllerNumero1.text = "";
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -59,13 +73,23 @@ class _CalculadoraState extends State<Calculadora> {
               decoration: InputDecoration(labelText: 'Digite um Nº'),
             ),
             SizedBox(height: 10.0),
-            ElevatedButton(
-              onPressed: () => _adivinhar(),
-              child: Text('Adivinhar'),
-            ),
+            if(!acertou)
+              ElevatedButton(
+                onPressed: () => _adivinhar(),
+                child: Text('Adivinhar'),
+              ),
             SizedBox(height: 16.0),
             Text(_resultado,
                 style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
+            
+            SizedBox(height: 10.0),
+              Visibility(
+                visible: acertou,
+                child: ElevatedButton(
+                onPressed: () => _reiniciar(),
+                child: Text('Reiniciar'),
+              )),
+              
           ],
         ),
       ),
