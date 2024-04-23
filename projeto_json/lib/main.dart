428d297440765348ca3f96ec54d9835ac5a0b5c4
Diogotb <digogo@gmail.com>
2024-04-23 18:14:53 -0300

    23/04

---
+++ projeto_json/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'View/HomeView.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Projeto Json",
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: HomePage(),
+    );
+  }
+}
