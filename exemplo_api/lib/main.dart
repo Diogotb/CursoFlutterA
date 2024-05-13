081a1cbbb5f5b8d5222235d22b019da7bf0d4e6a
Diogotb <digogo@gmail.com>
2024-05-13 16:40:47 -0300

    13/05

--- exemplo_api/lib/main.dart
+++ exemplo_api/lib/main.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
 
+
 import 'screen.dart';
 
-void main(){
+
+void main() {
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:4020234580.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Exemplo API',
+    return MaterialApp(
+      title: 'Weather Forecast',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: WeatherScreen(),
     );
   }
